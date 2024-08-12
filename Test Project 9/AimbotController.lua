6c30cd41e050ebd7ccb337a971d9e49baa9c060a
digit <125754881+diglt@users.noreply.github.com>
2024-08-12 19:07:37 +0100

    Create AimbotController.lua

---
+++ Test Project 9/AimbotController.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 17625359962 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/diglt/Luau-Scripts/main/Test%20Project%209/Rivals%20Aimbot.lua"))() 
+elseif game.PlaceId ~= 17625359962 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/diglt/Luau-Scripts/main/Test%20Project%209/Arsenal%20Aimbot.lua'))()
+end
