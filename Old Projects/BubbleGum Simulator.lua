67f0895ceeb05a6536877f0f92dcebf1ea2806dc
digit <125754881+diglt@users.noreply.github.com>
2024-08-13 02:17:30 +0100

    Create BubbleGum Simulator.lua

---
+++ Old Projects/BubbleGum Simulator.lua
@@ -0,0 +1,344 @@
+getgenv().Bbj = false
+getgenv().SF = false
+getgenv().GP = false
+getgenv().ER = false
+
+function doER()
+    spawn(function()
+        while ER == true do
+            if not getgenv() then break end
+            local New_CFrame = CFrame.new(-52, 228, 1512) 
+
+            local ts = game:GetService("TweenService")
+            local char = game.Players.LocalPlayer.Character
+        
+            local part = char.HumanoidRootPart
+        
+            local ti = TweenInfo.new(3, Enum.EasingStyle.Linear) -- 1 lower number = faster it goes
+            local tp = {CFrame = New_CFrame}
+            ts:Create(part, ti,tp):Play()
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 1}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 2}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 3}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 4}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 5}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 6}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 7}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 8}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 9}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 10}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 11}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 12}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 13}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 14}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 15}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 16}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 17}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 18}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 19}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 20}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 21}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 22}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 23}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 24}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 25}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 26}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 27}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 28}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 29}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 30}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 31}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 32}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 33}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 34}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 35}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 36}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 37}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 38}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 39}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 40}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 41}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 42}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 43}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 44}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 45}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 46}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 47}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 48}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 49}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            local args = {[1] = "CollectReward",[2] = "Christmas21",[3] = 50}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            wait()
+
+        end
+    end)
+end
+
+
+function doGP()
+    spawn(function()
+        while GP == true do
+            if not getgenv() then break end
+            local New_CFrame = CFrame.new(-177, 240, 1412) 
+
+            local ts = game:GetService("TweenService")
+            local char = game.Players.LocalPlayer.Character
+        
+            local part = char.HumanoidRootPart
+        
+            local ti = TweenInfo.new(3, Enum.EasingStyle.Linear) -- 1 lower number = faster it goes
+            local tp = {CFrame = New_CFrame}
+            ts:Create(part, ti,tp):Play()
+            wait(3)
+            local args = {[1] = "PurchaseEgg",[2] = "Frosted Egg"}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            wait()    
+        end
+    end)
+end
+
+
+function doBj()
+    spawn(function()
+        while Bbj == true do
+            local args = {[1] = "BlowBubble"}game:GetService("ReplicatedStorage"):WaitForChild("NetworkRemoteEvent"):FireServer(unpack(args))
+            wait()
+        end
+    end)
+end
+
+
+
+
+function doSF()
+    spawn(function()
+        while SF == true do
+            if not getgenv() then break end
+        local New_CFrame = CFrame.new(16, 280, 1611) 
+
+        local ts = game:GetService("TweenService")
+        local char = game.Players.LocalPlayer.Character
+    
+        local part = char.HumanoidRootPart
+    
+        local ti = TweenInfo.new(1, Enum.EasingStyle.Linear) -- 1 lower number = faster it goes
+        local tp = {CFrame = New_CFrame}
+        ts:Create(part, ti,tp):Play()
+        wait(3)
+
+
+        local New_CFrame = CFrame.new(76, 273, 1585)
+
+        local ts = game:GetService("TweenService")
+        local char = game.Players.LocalPlayer.Character
+    
+        local part = char.HumanoidRootPart
+    
+        local ti = TweenInfo.new(1, Enum.EasingStyle.Linear) -- 1 lower number = faster it goes
+        local tp = {CFrame = New_CFrame}
+        ts:Create(part, ti,tp):Play()
+        wait(3)
+
+
+        local New_CFrame = CFrame.new(85, 267, 1535) 
+
+        local ts = game:GetService("TweenService")
+        local char = game.Players.LocalPlayer.Character
+    
+        local part = char.HumanoidRootPart
+    
+        local ti = TweenInfo.new(1, Enum.EasingStyle.Linear) -- 1 lower number = faster it goes
+        local tp = {CFrame = New_CFrame}
+        ts:Create(part, ti,tp):Play()
+        wait(3)
+
+
+        local New_CFrame = CFrame.new(38, 271, 1486)
+
+        local ts = game:GetService("TweenService")
+        local char = game.Players.LocalPlayer.Character
+    
+        local part = char.HumanoidRootPart
+    
+        local ti = TweenInfo.new(1, Enum.EasingStyle.Linear) -- 1 lower number = faster it goes
+        local tp = {CFrame = New_CFrame}
+        ts:Create(part, ti,tp):Play()
+        wait(3)
+
+        local New_CFrame = CFrame.new(5, 258, 1472)
+
+        local ts = game:GetService("TweenService")
+        local char = game.Players.LocalPlayer.Character
+    
+        local part = char.HumanoidRootPart
+    
+        local ti = TweenInfo.new(1, Enum.EasingStyle.Linear) -- 1 lower number = faster it goes
+        local tp = {CFrame = New_CFrame}
+        ts:Create(part, ti,tp):Play()
+        wait(3)
+        end
+    end)
+end
+
+
+
+
+-- GUI
+
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+
+
+local Window = Rayfield:CreateWindow({
+    Name = "--",
+    LoadingTitle = "why are you reading this",
+    LoadingSubtitle = "by diglt",
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = nil, 
+       FileName = "Digital Hub"
+    },
+    Discord = {
+       Enabled = false,
+       Invite = "wk2xtM8H", 
+       RememberJoins = true 
+    },
+    KeySystem = false, 
+    KeySettings = {
+       Title = "",
+       Subtitle = "",
+       Note = "",
+       FileName = "",
+       SaveKey = true,
+       GrabKeyFromSite = false, 
+       Key = ""
+    }
+ })
+
+ local Tab = Window:CreateTab("Bubble Gum Simulator")
+ local Tab1 = Window:CreateTab("Other") 
+
+ 
+ local Button = Tab1:CreateButton({
+    Name = "Destroy Gui",
+    Callback = function()
+        Rayfield:Destroy()
+    end,
+ })
+
+ local Section = Tab:CreateSection("Main")
+
+local Toggle = Tab:CreateToggle({
+   Name = "Auto Blow ;)",
+   CurrentValue = false,
+   Callback = function(bool)
+    getgenv().Bbj = bool
+    if bool then
+        doBj()
+    end
+   end,
+})
+
+
+local Button = Tab:CreateButton({
+    Name = "Get Overworld Chests",
+    Callback = function()
+        local New_CFrame = game:GetService("Workspace").FloatingIslands.Overworld["The Floating Island"].Chest.FastSpawn.Ignore.CFrame -- put the path to the part here
+
+
+        local ts = game:GetService("TweenService")
+        local char = game.Players.LocalPlayer.Character
+       
+        local part = char.HumanoidRootPart
+       
+        local ti = TweenInfo.new(10, Enum.EasingStyle.Linear) -- 1 lower number = faster it goes
+        local tp = {CFrame = New_CFrame}
+        ts:Create(part, ti,tp):Play()
+        wait(15)
+        local New_CFrame = game:GetService("Workspace").FloatingIslands.Overworld["The Skylands"].Chest.FastSpawn.Ignore.CFrame -- put the path to the part here
+
+
+        local ts = game:GetService("TweenService")
+        local char = game.Players.LocalPlayer.Character
+       
+        local part = char.HumanoidRootPart
+       
+        local ti = TweenInfo.new(10, Enum.EasingStyle.Linear) -- 1 lower number = faster it goes
+        local tp = {CFrame = New_CFrame}
+        ts:Create(part, ti,tp):Play()
+        wait(15)
+        local New_CFrame = game:GetService("Workspace").FloatingIslands.Overworld["The Void"].Chest.FastSpawn.Ignore.CFrame -- put the path to the part here
+
+
+        local ts = game:GetService("TweenService")
+        local char = game.Players.LocalPlayer.Character
+       
+        local part = char.HumanoidRootPart
+       
+        local ti = TweenInfo.new(10, Enum.EasingStyle.Linear) -- 1 lower number = faster it goes
+        local tp = {CFrame = New_CFrame}
+        ts:Create(part, ti,tp):Play()
+        wait(15)
+        local New_CFrame = game:GetService("Workspace").FloatingIslands.Overworld["XP Island"].Chest.FastSpawn.Ignore.CFrame -- put the path to the part here
+
+
+        local ts = game:GetService("TweenService")
+        local char = game.Players.LocalPlayer.Character
+       
+        local part = char.HumanoidRootPart
+       
+        local ti = TweenInfo.new(10, Enum.EasingStyle.Linear) -- 1 lower number = faster it goes
+        local tp = {CFrame = New_CFrame}
+        ts:Create(part, ti,tp):Play()
+        wait(15)
+    
+    end,
+ })
+
+ local Section = Tab:CreateSection("Snowflake Event In Febuary")
+
+ local Button = Tab:CreateButton({
+    Name = "Go To Snowflake Portal (Use before anything else below)",
+    Callback = function()
+        local New_CFrame = CFrame.new( 81, 46, -311) -- put the path to the part here
+        local ts = game:GetService("TweenService")
+        local char = game.Players.LocalPlayer.Character
+        local part = char.HumanoidRootPart
+        local ti = TweenInfo.new(5, Enum.EasingStyle.Linear) -- 1 lower number = faster it goes
+        local tp = {CFrame = New_CFrame}
+        ts:Create(part, ti,tp):Play()
+    end,
+ })
+
+
+ local Toggle = Tab:CreateToggle({
+    Name = "Auto Open Pet",
+    CurrentValue = false,
+    Callback = function(bool)
+     getgenv().GP = bool
+     if bool then
+         doGP()
+     end
+    end,
+ })
+
+ local Toggle = Tab:CreateToggle({
+    Name = "Auto Buy Rewards",
+    CurrentValue = false,
+    Callback = function(bool)
+     getgenv().ER = bool
+     if bool then
+         doER()
+     end
+    end,
+ })
+
+
+
+ local Toggle = Tab:CreateToggle({
+    Name = "Auto Get Snowflakes",
+    CurrentValue = false,
+    Callback = function(bool)
+     getgenv().SF = bool
+     if bool then
+         doSF()
+     end
+    end,
+ })
